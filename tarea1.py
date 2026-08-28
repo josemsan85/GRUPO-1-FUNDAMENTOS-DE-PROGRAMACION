5fb1aacbf559712da90a67a8da1dae2306268319
ynumazen <51127000+ynumazen@users.noreply.github.com>
2026-08-28 16:27:14 -0500

    Add files via upload
    algoritmo fórmula donde se muestra el área y perímetro de un rectángulo. Los lados del rectángulo pueden ser colocados por el usuario.

---
+++ tarea1.py
@@ -0,0 +1,5 @@
+ancho = float (input ("Ancho del rectangulo "))
+largo = float (input ("Largo del rectangulo "))
+
+print ("el area del rectangulo es " + str( ancho*largo))
+print ("su perimetro es " + str( (ancho +largo) *2 ) )
