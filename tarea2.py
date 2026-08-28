7758c3b39ae9e4f6e3e9f6f4c99b5b36e001f4c1
ynumazen <51127000+ynumazen@users.noreply.github.com>
2026-08-28 16:29:26 -0500

    Add files via upload

---
+++ tarea2.py
@@ -0,0 +1,16 @@
+num1 = int (input("Primer  numero ")) 
+num2 = int (input("Segundo numero ")) 
+
+print 
+
+if num1<num2:
+   
+
+  print(str(num1)+" es menor que "+str(num2))
+
+elif num1 > num2:
+  print(str(num1)+" es mayor que "+str(num2))
+  
+elif num1 == num2 :
+
+  print ("ambos numeros " + str (num1) + " son iguales")
